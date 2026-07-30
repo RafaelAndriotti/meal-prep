23e0fd76c0a05bdd3190fae752018ca26df8135e
rafaelandriotti <rafael.rebelo47@gmail.com>
2026-07-30 00:08:34 -0300

    Criando tabelas faltantes

---
+++ src/migrations/20260730025917-create-recipe-ingredients.cjs
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('RecipeIngredients', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      recipe_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Recipes',
+          key: 'id'
+        }
+      },
+      ingredient_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Ingredients',
+          key: 'id'
+        }
+      },
+      quantity: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('RecipeIngredients');
+  }
+};
---
+++ src/models/recipe_ingredients.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class RecipeIngredients extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  RecipeIngredients.init({
+    recipe_id: DataTypes.INTEGER,
+    ingredient_id: DataTypes.INTEGER,
+    quantity: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'RecipeIngredients',
+  });
+  return RecipeIngredients;
+};
